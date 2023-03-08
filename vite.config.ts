c317f349be239913786724b47675896b8a9dd433
hehe19950110 <18807378000@139.com>
2023-03-08 10:23:12 +0800

    添加base 路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mangosteen/",
+  base: "/mangosteen-1/dist/",
   plugins: [vue()],
 });

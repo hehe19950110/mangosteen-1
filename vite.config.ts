7102b86aadefe60003f10ee74abb3ae3d9cf4d20
hehe19950110 <18807378000@139.com>
2023-03-08 10:52:59 +0800

    ignore dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mangosteen-1/dist/",
+  // base: "/mangosteen-1/dist/",
   plugins: [vue()],
 });

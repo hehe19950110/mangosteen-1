990b5d7867a3a16e9cc2413ce3ccd44232e4985f
hehe19950110 <18807378000@139.com>
2023-03-19 19:39:58 +0800

    新增center 与 navbar组件

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,6 +1,9 @@
 import { defineComponent } from 'vue';
 import { Button } from '../shared/buttons/Button';
 import { FloatButton } from '../shared/buttons/FloatButton';
+import { Center } from '../shared/Center';
+import { Icon } from '../shared/Icon';
+import { Navbar } from '../shared/Navbar';
 import style from './StartPage.module.scss';
 
 export const StartPage = defineComponent({ 
@@ -11,9 +14,23 @@ export const StartPage = defineComponent({
     
     return () => (
       <div>
+        <Navbar>
+          {
+            {
+              default: () => '山竹记账',
+              icon: () => <Icon name='menu' class={style.navIcon} />
+            }
+          }
+        </Navbar>
+
+        <Center class={style.pig_wrapper}>
+          <Icon name='pig' class={style.pig} />
+        </Center>
+
         <div class={style.button_wrapper}>
           <Button class={style.button} onClick={onClick} >button测试</Button>
         </div>
+
         <FloatButton iconName='add' />
       </div>
     )
--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,22 +1,22 @@
 import { defineComponent, PropType } from 'vue';
 import style from './Icon.module.scss';
 
-// interface Props {
-//   name: 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig';
-// }
-
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export'
 
 export const Icon = defineComponent({
   props: {
     name: {
       type: String as PropType<IconName>,
       required: true,
+    },
+    onClick: {
+      type: Function as PropType<(e:MouseEvent) => void>
     }
   },
+
   setup: (props, context) => {
     return () => (
-      <svg class={style.icon}>
+      <svg class={style.icon} onClick={props.onClick}>
         <use xlinkHref={'#' + props.name}></use>
       </svg>
     )

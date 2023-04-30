df6cf210184871ea12f57b5c2b3422ea1e9c9a3b
hehe19950110 <18807378000@139.com>
2023-04-30 15:37:00 +0800

    敬请期待页面menu

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,17 +1,34 @@
 import { defineComponent, PropType } from "vue";
+import { RouterLink } from "vue-router";
+import { LayoutNavBar } from "../layouts/LayoutNavBar";
+import { FloatButton } from "./buttons/FloatButton";
 import { Center } from "./Center";
 import style from "./ComingSoon.module.scss";
 import { Icon } from "./Icon/Icon";
+import { Navbar } from "./Navbar";
+import { OverlayIcon } from "./Overlay";
 
 export const ComingSoon = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <Center class={style.pig_wrapper}>
-          <Icon name="pig" class={style.pig} />
-        </Center>
-        <p class={style.text}>敬请期待</p>
-      </div>
+      <LayoutNavBar>
+        {{
+          title: () => "月月记账",
+          icon: () => <OverlayIcon />,
+          default: () => (
+            <>
+              <Center class={style.pig_wrapper}>
+                <Icon name="pig" class={style.pig} />
+              </Center>
+              <p class={style.text}> 敬请期待 </p>
+
+              <RouterLink to="/items/create">
+                <FloatButton iconName="add" />
+              </RouterLink>
+            </>
+          ),
+        }}
+      </LayoutNavBar>
     );
   },
 });
